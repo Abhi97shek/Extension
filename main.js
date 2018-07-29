f47be8ef38b4eb47f01e954443c86467cfa2be19
Abhi97shek <abhi.kumar736@gmail.com>
2018-07-29 23:09:13 +0530

    Final Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".fel #ft_un").val("15BCS1927");
+  $(".fel #ft_pd").val("Abhi.123@");
+
+  $("input[type='submit']").trigger('click');
+  // $("#btn").trigger('click');
+});
